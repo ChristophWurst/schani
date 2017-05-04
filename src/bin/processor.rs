3139320a9435a3dfca1ad79d713fd8c94fc8ad5e
Christoph Wurst <christoph@winzerhof-wurst.at>
2017-05-04 20:49:16 +0200

    Add Rocket to build simple web service
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- src/bin/processor.rs
+++ src/bin/processor.rs
@@ -1,13 +1,25 @@
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+
 extern crate schani;
+extern crate rocket;
 
 use schani::core::images::RawtherapeeImage;
 use schani::processing::process_raw;
 
-fn main() {
+#[post("/process/<file>")]
+fn index(file: &str) -> &'static str {
+    print!("procesing raw file {}", file);
+
     let image = RawtherapeeImage {
         name: "hello".to_string(),
-        raw: "resources/DSC_2936.NEF".to_string(),
+        raw: "resources/".to_string() + &file,
         sidecar: "resources/DSC_2936.NEF.pp3".to_string(),
     };
     process_raw(&image);
+    "file processed"
+}
+
+fn main() {
+    rocket::ignite().mount("/", routes![index]).launch();
 }
--- src/processing/mod.rs
+++ src/processing/mod.rs
@@ -4,11 +4,11 @@ use std::process::Command;
 
 pub fn process_raw(img: &RawtherapeeImage) {
     // rawtherapee -j90 -Y -c resources/DSC_2936.NEF
-    let out = Command::new("./convert.sh")
+    Command::new("./convert.sh")
         .arg(&img.raw)
         .output()
         .expect("failed to start Rawtherapee");
 
-    println!("std output: {}", String::from_utf8_lossy(&out.stdout));
-    println!("err output: {}", String::from_utf8_lossy(&out.stderr));
+    // println!("std output: {}", String::from_utf8_lossy(&out.stdout));
+    // println!("err output: {}", String::from_utf8_lossy(&out.stderr));
 }
